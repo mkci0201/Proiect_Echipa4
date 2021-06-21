28d2603def58d62dc03ac0f3ea9424b80a93f536
mkci0201 <mkci0201@scs.ubbcluj.ro>
2021-06-21 11:07:30 +0300

    shopOrder Stock impl

--- flowershop-web/src/webapp/src/app/shoporder/shoporder-new/shoporder-new.component.ts
+++ flowershop-web/src/webapp/src/app/shoporder/shoporder-new/shoporder-new.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, ViewChild} from '@angular/core';
 import {Category, CATEGORY_TYPES} from "../shared/shoporder.model";
 import {Location} from '@angular/common';
 import {ShopOrderService} from "../shared/shoporder.service";
@@ -26,6 +26,9 @@ export class ShoporderNewComponent {
   errorMessage: string;
   selectedProduct : Product;
   employee: Employee;
+  stockError : any={errorActive:false, errorMessage:''};
+
+  @ViewChild('orderQuantity') orderQuantity;
 
   constructor(private shopOrderService: ShopOrderService, private productService : ProductService,
               private orderedProductsService : OrderedProductService,
@@ -60,7 +63,17 @@ export class ShoporderNewComponent {
 
 
   placeOrder(date, category): void {
-    this.shopOrderService.addOrder(date, category, this.orderedProducts, this.employee).subscribe(_=>this.goBack());
+    this.shopOrderService.addOrder(date, category, this.orderedProducts, this.employee).subscribe(v => this.orderPlacedSuccessfully(v));
+  }
+
+  orderPlacedSuccessfully(shopOrder) {
+
+    for (let p of shopOrder.orderedProducts) {
+      let product = p.product;
+      product.stock -= p.quantity;
+      this.productService.updateProduct(product).subscribe();
+    }
+    this.goBack();
   }
 
   addProduct(quantity) : void {
@@ -72,23 +85,56 @@ export class ShoporderNewComponent {
     let quantityAsNumber : number = +quantity;
 
     if(foundProduct != null){
-      foundProduct.quantity += quantityAsNumber;
-      this.orderedProductsService.updateOrderedProduct(foundProduct);
+      if (foundProduct.quantity + quantityAsNumber > foundProduct.product.stock)
+      {
+        this.stockError.errorActive = true;
+        this.stockError.errorMessage = 'There are not enough products on stock!';
+      }
+      else
+      {
+        foundProduct.quantity += quantityAsNumber;
+        this.orderedProductsService.updateOrderedProduct(foundProduct);
+      }
     } else {
       var orderedProduct: OrderedProduct = {id:0, product:this.selectedProduct, quantity: quantityAsNumber};
       this.orderedProductsService.addOrderedProduct(orderedProduct.product, orderedProduct.quantity).subscribe((value:OrderedProduct)=>this.orderedProducts.push(value));
     }
   }
 
-
-
   onChange(newValue) {
     this.selectedProduct = this.products.find(p => p.id == newValue);
+    this.stockError.errorActive = false;
+    if (this.orderQuantity)
+      this.orderQuantity.nativeElement.value = '1';
   }
 
   get totalPrice(){
     let total : number = 0;
     this.orderedProducts.forEach(a => total += a.product.price * a.quantity);
     return total;
+  }
+
+  delete(orderedProduct : OrderedProduct) {
+    this.stockError.errorActive = false;
+    if (this.orderQuantity)
+      this.orderQuantity.nativeElement.value = '1';
+    var foundProduct = this.orderedProducts.find(p => p.product.id == orderedProduct.product.id);
+    if (foundProduct) {
+      this.orderedProductsService.deleteOrderedProduct(orderedProduct.id).subscribe();
+      let index = this.orderedProducts.findIndex(obj => obj.id == orderedProduct.id);
+      this.orderedProducts.splice(index, 1);
+    }
+  }
+
+  onSelectedProductStock(stock : number, selectedStock) {
+    if (+selectedStock > stock) {
+      this.stockError.errorActive = true;
+      this.stockError.errorMessage = 'There are not enough products on stock';
+    }
+    else
+    {
+      this.stockError.errorActive = false;
+      this.stockError.errorMessage = '';
+    }
   }
 }
